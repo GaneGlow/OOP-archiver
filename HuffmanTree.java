84b1f5684eab133472bf76d865fa9b1f2d305000
Gane Glow <xxxsofochka@mail.com>
2025-09-26 19:03:52 +0300

    Add method buildTree to HuffmanTree class

--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,3 +1,24 @@
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class HuffmanTree {
 
+    public static HuffmanTreeNode buildTree(Map<Byte, Integer> treeValues) {
+        PriorityQueue<HuffmanTreeNode> queue = new PriorityQueue<>();
+
+        for(Map.Entry<Byte, Integer> entry : treeValues.entrySet()) {
+            queue.offer(new HuffmanTreeNode(entry.getKey(), entry.getValue()));
+        }
+
+        while (queue.size() > 1) {
+            HuffmanTreeNode left = queue.poll();
+            HuffmanTreeNode right = queue.poll();
+            assert right != null;
+            HuffmanTreeNode parent = new HuffmanTreeNode(left.frequency + right.frequency, left, right);
+
+            queue.offer(parent);
+        }
+
+        return queue.poll();
+    }
 }
